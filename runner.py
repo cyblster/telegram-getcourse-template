021c01884a531395340e90cfd96a789d5d4e2ac9
fadeevdan77 <37071085+cyblster@users.noreply.github.com>
2022-08-17 17:37:38 +0500

    repo: fix 17.08.2022

--- runner.py
+++ runner.py
@@ -3,6 +3,6 @@ from app import conf, app, bot
 
 if __name__ == '__main__':
     bot.remove_webhook()
-    bot.set_webhook(url=conf.APP_URL)
+    bot.set_webhook(url=f'{conf.APP_URL}/{conf.BOT_KEY}')
 
     app.run(host=conf.HOST, port=conf.PORT)
