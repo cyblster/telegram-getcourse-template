015ddee47ed9ac7402d5a9b94f1ca65b58525e23
fadeevdan77 <37071085+cyblster@users.noreply.github.com>
2022-08-17 17:54:52 +0500

    repo: fix 17.08.2022

--- app/handlers.py
+++ app/handlers.py
@@ -16,14 +16,16 @@ def message_email(message):
     telegram_lastname = message.from_user.last_name
 
     for payment_rate, telegram_id in mysql_query(f'SELECT payment_rate, telegram_id FROM customer WHERE email = "{email}"'):
-        channel_link = bot.create_chat_invite_link(chat_id=conf.APP_RATE[payment_rate], expire_date=86400, member_limit=1).invite_link
+        invite_link = bot.create_chat_invite_link(chat_id=conf.APP_RATE[payment_rate], expire_date=86400, member_limit=1).invite_link
 
-        mysql_query(f'UPDATE customer SET telegram_username = "{telegram_username}" telegram_firstname = "{telegram_firstname}", telegram_lastname = "{telegram_lastname}", channel_link = "{channel_link}"')
+        mysql_query(f'UPDATE customer SET telegram_username = "{telegram_username}" telegram_firstname = "{telegram_firstname}", telegram_lastname = "{telegram_lastname}", invite_link = "{invite_link}"')
 
         inline_keyboard = types.InlineKeyboardMarkup()
         inline_keyboard.add(types.InlineKeyboardButton(text='Перейти на канал', callback_data='channel_referer_button'))
 
         bot.send_message(chat_id=telegram_id, text='123', reply_markup=inline_keyboard)
+
+    bot.send_message(chat_id=message.from_user.id, text='success')
 
 
 @bot.callback_query_handler(func=lambda call: call.data == 'channel_referer_button')
